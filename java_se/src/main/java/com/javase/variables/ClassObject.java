142c7f3e948985634b41dc93052dc8434e6d7fbb
Bryan <bryanshubo@gmail.com>
2015-06-01 17:03:21 -0400

    add variables section

---
+++ java_se/src/main/java/com/javase/variables/ClassObject.java
@@ -0,0 +1,17 @@
+package com.javase.variables;
+
+/**
+ * Created by uc0006 on 6/1/2015.
+ */
+public class ClassObject {
+
+    // The method clone
+
+    // The method equals
+
+    // The method finalize
+
+    // The method getClass
+
+    // The 
+}
---
+++ java_se/src/main/java/com/javase/variables/PrimitiveRange.java
@@ -0,0 +1,43 @@
+package com.javase.variables;
+
+/**
+ * Created by uc0006 on 6/1/2015.
+ */
+public class PrimitiveRange {
+
+    public static void main(String[] args) {
+
+        System.out.println("Byte type value range: ");
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println();
+        System.out.println("Short type value range: ");
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        //2^31-1 and -2^31
+        System.out.println();
+        System.out.println("Integer type value range: ");
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println();
+        System.out.println("Long type value range: ");
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println();
+        System.out.println("Float type value range: ");
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println();
+        System.out.println("Double type value range: ");
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+
+
+
+    }
+}
