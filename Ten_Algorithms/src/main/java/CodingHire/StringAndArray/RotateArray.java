1b1a0e2936cd6b0e34b0d2fe93036860d45efebf
Shubo Zhang <bryanshubo@gmail.com>
2015-07-14 00:20:39 -0400

    first push

---
+++ Ten_Algorithms/src/main/java/CodingHire/StringAndArray/RotateArray.java
@@ -0,0 +1,31 @@
+package CodingHire.StringAndArray;
+
+/**
+ * Problem: Rotate an array of n elements to the right by k steps.
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
+ * [5,6,7,1,2,3,4]. How many different ways do you know to solve this problem?
+ */
+public class RotateArray {
+
+    public static void main(String[] args){
+
+    }
+
+    public static int[] rotate(int[] arr, int k) {
+
+        reverse(arr, 0, arr.length - k -1);
+        reverse(arr, arr.length - k, arr.length);
+
+        return null;
+    }
+
+    public static int[] reverse(int[] arr, int start, int end) {
+        for(int i = start; i < end - start; i++) {
+            int temp = arr[i];
+            arr[i] = arr[end - i];
+            arr[end - i] = temp;
+        }
+        return arr;
+    }
+
+}
