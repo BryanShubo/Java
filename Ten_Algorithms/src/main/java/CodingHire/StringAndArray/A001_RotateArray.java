6cf9842133e193b2dba6e0b2bb0e1ee032e5776c
Bryan Zhang <bryanshubo@gmail.com>
2015-08-31 14:53:44 -0400

    push A001_RotateArray

--- Ten_Algorithms/src/main/java/CodingHire/StringAndArray/A001_RotateArray.java
+++ Ten_Algorithms/src/main/java/CodingHire/StringAndArray/A001_RotateArray.java
@@ -1,31 +1,35 @@
 package CodingHire.StringAndArray;
 
 /**
- * Problem: Rotate an array of n elements to the right by k steps.
- * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
+ * Problem: Rotate an array of n elements to the right by k steps. For example,
+ * with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to
  * [5,6,7,1,2,3,4]. How many different ways do you know to solve this problem?
  */
 public class A001_RotateArray {
 
-    public static void main(String[] args){
+	public static void main(String[] args) {
 
-    }
+		int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+		int k = 3;
 
-    public static int[] rotate(int[] arr, int k) {
+		int[] reverse = rotate(array, k);
 
-        reverse(arr, 0, arr.length - k -1);
-        reverse(arr, arr.length - k, arr.length);
+		for (int i = 0; i < reverse.length; i++) {
+			System.out.print(reverse[i] + " ");
+		}
+	}
 
-        return null;
-    }
+	public static int[] rotate(int[] arr, int k) {
+		int length = arr.length;
+		int[] temp = new int[length];
 
-    public static int[] reverse(int[] arr, int start, int end) {
-        for(int i = start; i < end - start; i++) {
-            int temp = arr[i];
-            arr[i] = arr[end - i];
-            arr[end - i] = temp;
-        }
-        return arr;
-    }
+		for (int i = 0; i < length - k; i++) {
+			temp[i + k] = arr[i];
+		}
+		for (int j = length - k; j < length; j++) {
+			temp[j - (length - k)] = arr[j];
+		}
 
+		return temp;
+	}
 }
